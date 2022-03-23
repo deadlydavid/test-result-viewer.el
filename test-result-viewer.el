abd0be23f17dfa98b7364891924aa033d5f03a4d
dadi261776 <david.dionis@elektrobit.com>
2022-03-23 21:36:31 +0100

    initial commit with very basic version

---
+++ test-result-viewer.el
@@ -0,0 +1,29 @@
+(require 'xml)
+
+(get-buffer-create "*test-results*")
+
+(defun print-testcase (testcase)
+  "prints a testcase into the test-results buffer"
+    (with-current-buffer "*test-results*"
+      (goto-char (point-max))
+      (if (or (xml-get-children testcase 'failure)
+	      (xml-get-children testcase 'error))
+	  (insert "[x] ") (insert "[o] "))
+      (insert (substring (prin1-to-string (dom-attr testcase 'name)) 1 -1)) (insert " (")
+      (insert (substring (prin1-to-string (dom-attr testcase 'time)) 1 -1)) (insert " sec)\n")))
+
+(defun print-all-testcases (list-of-testcases)
+  "prints all testcases contained in the given list"
+  (progn (with-current-buffer "*test-results*" (erase-buffer))
+	 (let (value) (dolist (elt list-of-testcases) (print-testcase elt)))))
+
+(defun print-all-testcases-from-file (filename)
+  "prints all testcases from the report file into the buffer *test-results*"
+  (print-all-testcases (xml-get-children (assq 'testsuite (xml-parse-file filename)) 'testcase)))
+
+(defun print-and-show-testcases-in-buffer () (interactive)
+ (progn (print-all-testcases-from-file test-result-viewer-reportfile)
+	(display-buffer-in-side-window (get-buffer "*test-results*") '((side . right)))))
+
+(global-set-key [f9] 'print-and-show-testcases-in-buffer)
+
